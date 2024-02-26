591fcc250317d8c0b79d62565f91b2298b6fa6e5
umairrazam <omairrr.azam@gmail.com>
2024-02-26 16:26:30 +0500

    feat(gem): spree version

--- app/models/spree/state_decorator.rb
+++ app/models/spree/state_decorator.rb
@@ -1,3 +1,8 @@
-Spree::State.class_eval do
-  has_many :counties, -> { order('name ASC') }, dependent: :destroy
+module Spree
+  module StateDecorator
+    def self.prepended(base)
+      base.has_many :counties, -> { order('name ASC') }, dependent: :destroy
+    end
+  end
 end
+::Spree::State.prepend Spree::StateDecorator if ::Spree::State.included_modules.exclude?(Spree::StateDecorator)
--- spree_counties.gemspec
+++ spree_counties.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |s|
   s.require_path = 'lib'
   s.requirements << 'none'
 
-  spree_version = '~> 4.7'
+  spree_version = '>= 4.3'
 
   s.add_dependency('spree_core', spree_version)
   s.add_dependency('spree_backend', spree_version)
