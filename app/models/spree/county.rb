48f5c07382432466384579968de8967c351f9f75
umairr.azam <omairrr.azam@gmail.com>
2024-02-29 22:11:01 +0500

    fix(counties-api): Counties api revamp

--- app/models/spree/county.rb
+++ app/models/spree/county.rb
@@ -1,12 +1,21 @@
 module Spree
   class County < ActiveRecord::Base
-
     belongs_to :state
     has_many :addresses, dependent: :nullify
 
     validates :name, presence: true
     
     Spree::Ability.register_ability(CountyAbility)
+
+    def self.ransackable_attributes(auth_object = nil)
+      ["created_at", "id", "id_value", "name", "state_id", "updated_at", "zipcode"]
+    end
+
+    def self.json_api_permitted_attributes
+      [
+        'id', 'name', 'state_id', 'zipcode'
+      ]
+    end
 
     def <=>(other)
       name <=> other.name
